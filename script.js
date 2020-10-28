c0bdb6badb772e7930fdd1b68b6399dc6978634f
Olivia Wu <qinyuan.wupsn@gmail.com>
2020-10-28 19:45:10 +0800

    countries populated

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("This is quiz-homework!")
+
+const requestOptions = {
+  method: "GET",
+  redirect: "follow",
+};
+
+fetch(
+    `https://restcountries.eu/rest/v2/all`,
+    requestOptions
+  )
+    .then((response) => response.json())
+    .then((data) => populateSelector("countires", data))
+    .catch((error) => console.log("error", error));
+
+function populateSelector(selectElmId, data) {
+  const select = document.getElementById(selectElmId);
+  select.innerHTML = "";
+  let item = null;
+  for (let i = 0; i < data.length; i++) {
+    item = document.createElement("option");
+    item.value = data[i]["name"];
+    item.innerText = data[i]["name"];
+    select.appendChild(item);
+  }
+}
+
