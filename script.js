a8142d8c01ba836cf1bb28b0855e023bebdd22ca
Olivia Wu <qinyuan.wupsn@gmail.com>
2020-10-28 21:42:19 +0800

    real time data displaying

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const requestOptions = {
   redirect: "follow",
 };
 
+//get country data and populate country selector
 fetch(
     `https://restcountries.eu/rest/v2/all`,
     requestOptions
@@ -18,9 +19,54 @@ function populateSelector(selectElmId, data) {
   let item = null;
   for (let i = 0; i < data.length; i++) {
     item = document.createElement("option");
-    item.value = data[i]["name"];
-    item.innerText = data[i]["name"];
+    item.value = data[i]["alpha2Code"]; //store country code as value
+    item.innerText = data[i]["name"]; //store country name for display
     select.appendChild(item);
   }
 }
 
+//wait for user to select a country
+document.getElementById("countires").addEventListener("click", search);
+
+function search() {
+	const code = document.getElementById("countires").value;
+	const country = document.getElementById("countires").innerText;
+
+	fetch(
+	    `https://www.trackcorona.live/api/countries/${code}`,
+	    requestOptions
+	  )
+	    .then((response) => response.json())
+	    .then((data) => showSearchResults(data, code))
+	    .catch((error) => console.log("error", error));
+}
+
+function showSearchResults(data, code) {
+	let confirmed = document.getElementById("confirmed");
+	let recovered = document.getElementById("recovered");
+	let active = document.getElementById("active");
+
+
+	if (data["data"][0] != null) {
+
+		const num_confirmed = data["data"][0]["confirmed"];
+		confirmed.innerHTML = `<img src="https://www.countryflags.io/${code}/flat/24.png" /> ${num_confirmed.toLocaleString()}`;
+
+		
+		const num_recovered = data["data"][0]["recovered"];
+		recovered.innerHTML = `<img src="https://www.countryflags.io/${code}/flat/24.png" /> ${num_recovered.toLocaleString()}`;
+
+		
+		const num_active = num_confirmed - num_recovered - data["data"][0]["dead"];
+		active.innerHTML = `<img src="https://www.countryflags.io/${code}/flat/24.png" /> ${num_active.toLocaleString()}`;
+
+		console.log(`con=${num_confirmed}, rec=${num_recovered}, act=${num_active}`);
+	} else {
+		console.log(`no results found for ${code}`);
+		confirmed.innerHTML = `<img src="https://www.countryflags.io/${code}/flat/24.png" /> N/A`;
+		recovered.innerHTML = `<img src="https://www.countryflags.io/${code}/flat/24.png" /> N/A`;
+		active.innerHTML = `<img src="https://www.countryflags.io/${code}/flat/24.png" /> N/A`;
+	}
+	
+
+}
